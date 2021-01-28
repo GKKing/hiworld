760f1204ea6587da05a729c95b9d0dbf5534bd41
gkking <1932856602@qq.com>
2021-01-28 11:07:53 +0800

    test

---
+++ test1/src/day01/demo2/hellojava.java
@@ -0,0 +1,20 @@
+package day01.demo2;
+
+public class hellojava {
+    public static void main(String[] args) {
+        print1();
+    }
+
+
+    public static void print1(
+
+    ) {
+        for (int i = 0; i < 5; i++) {
+            for (int i1 = 0; i1 < 8; i1++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
